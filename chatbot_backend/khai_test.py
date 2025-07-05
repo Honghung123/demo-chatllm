026769fc65f5de5d2493e6b45da389257e94770a
vhkhai <vuonghuynhkhai4869@gmail.com>
2025-07-05 16:52:50 +0700

    enhance preprocessing data

--- chatbot_backend/khai_test.py
+++ chatbot_backend/khai_test.py
@@ -1,85 +1,39 @@
+from datetime import datetime
 import uuid
-from app.files.file_loader import load_file
-from app.files.file_metadata_manager import add_metadata, get_list_file_names_by_user_and_role, load_metadata, update_metadata
-from app.files.file_pre_processing import preprocess_text
-from app.search.document import Document
-from app.search.vector_db import ChromaManager
-
-DATA_DIR = "data/documents"
-METADATA_FILE_PATH = "data/metadata/file_metadata.json"
-# content = load_file(f'{DATA_DIR}/python.pdf')
-# chunks = preprocess_text(content)
-# print(f"Content of 'python.pdf':\n")
-# for i, chunk in enumerate(chunks):
-#     print(f"Chunk {i+1}:\n{chunk}\n")
+from common.file.file_loader import load_file
+from common.file.file_metadata_manager import add_metadata, get_list_file_names_by_user_and_role, load_metadata, update_metadata, get_list_personal_files, get_list_system_files
+from common.file.file_pre_processing import preprocess_text
+from common.search.document import Document
+from common.search.vector_db import chroma_db
 
 # add_metadata(
-#     file_name="python.pdf", author="admin", roles=["developer", "data scientist"])
-
-# update_metadata(
-#     file_name="python.pdf",
-#     author="user1",
-#     roles=["developer", "data scientist"]
+#     file_name="Monthly Sales Report.pdf", 
+#     extension="pdf", 
+#     original_name="Monthly Sales Report.pdf", 
+#     username="admin",
+#     timestamp=datetime.now().isoformat(),
+#     roles=["sales", "admin"]
 # )
 
-# print(f'list files for user1 and role sales {get_list_file_names_by_user_and_role("admin", "admin")}')
-
-chroma_db = ChromaManager(collection_name="my_documents", persist_directory="data/vector_store")
-print("Total documents in ChromaDB:", len(chroma_db.get_all_documents()))
+# print("Total documents in ChromaDB:", len(chroma_db.get_all_documents()))
 # chroma_db.clear_collection()
 
-# # add python.pdf to ChromaDB
-# content = load_file(f'{DATA_DIR}/python.pdf')
-# add_metadata(file_name="python.pdf", author="admin", roles=["admin", "hr", "sales"])
+# content = load_file(f'Marketing Campaign Plan.pdf')
 # chunks = preprocess_text(content)
+# print(f"number of chunks: {len(chunks)}")
 # docs = []
 # for i, chunk in enumerate(chunks):
 #     doc = Document(
 #         id=uuid.uuid4(),
 #         content=chunk,
 #         metadata={
-#             "filename": "python.pdf",
-#         }
-#     )
-#     docs.append(doc)
-# doc_ids = chroma_db.add_documents(docs)
-# print(f"Added {len(doc_ids)} documents to ChromaDB.")
-
-# # add History of Java.docx to ChromaDB
-# content = load_file(f'{DATA_DIR}/History of Java.docx')
-# add_metadata(file_name="History of Java.docx", author="admin", roles=["admin", "tech", "marketing"])
-# chunks = preprocess_text(content)
-# docs = []
-# for i, chunk in enumerate(chunks):
-#     doc = Document(
-#         id=uuid.uuid4(),
-#         content=chunk,
-#         metadata={
-#             "filename": "History of Java.docx",
+#             "filename": "Marketing Campaign Plan.pdf",
 #         }
 #     )
 #     docs.append(doc)
 # doc_ids = chroma_db.add_documents(docs)
 
-# add History of Java.docx to ChromaDB
-# content = load_file(f'{DATA_DIR}/c-sharp.docx')
-# add_metadata(file_name="c-sharp.docx", author="user1", roles=[])
-# chunks = preprocess_text(content)
-# docs = []
-# for i, chunk in enumerate(chunks):
-#     doc = Document(
-#         id=uuid.uuid4(),
-#         content=chunk,
-#         metadata={
-#             "filename": "c-sharp.docx",
-#         }
-#     )
-#     docs.append(doc)
-# doc_ids = chroma_db.add_documents(docs)
-
-
-# list_files = get_list_file_names_by_user_and_role("user1", "hr")
-# print(f"List of files for user1 and role hr: {list_files}")
-# docs = chroma_db.search_relative_documents(query="learning java programming", n_results=10, filenames=list_files)
-# print(f"Search results for 'learning java programming': {docs}")
-
+# list_files = get_list_file_names_by_user_and_role("admin", "admin")
+# print(f"List of files': {list_files}")
+# file_names = chroma_db.search_relative_documents(query="digital marketing", n_results=10, filenames=list_files)
+# print(f"Search results: {file_names}")
